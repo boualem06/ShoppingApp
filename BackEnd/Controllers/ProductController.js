80199f5ec760683272044bb8089b808f8801fb63
boualem06 <kb_hamroune@esi.dz>
2022-10-13 07:28:54 +0100

    finishing the backend of add product and get products

--- BackEnd/Controllers/ProductController.js
+++ BackEnd/Controllers/ProductController.js
@@ -1,11 +1,11 @@
-const ProductModel=require('../models/ProductModel')
+const ProductModel = require('../models/ProductModel')
 const multer = require("multer");
 const express = require('express')
 const router = express.Router();
 const fs = require("fs");
 const ImageModel = require("../models/ImageModel");
-const path=require('path') ;
-const { upload } =require('../midleware/imageMidleware') ;
+const path = require('path');
+const { upload } = require('../midleware/imageMidleware');
 //adding an image 
 
 
@@ -13,35 +13,47 @@ const { upload } =require('../midleware/imageMidleware') ;
 
 // const asyncHandler=require('express-async-handler') ;
 
-const addProduct=async(req,res)=>{
-    const saveImage =  ImageModel({
+const addProduct = async (req, res) => {
+    const saveImage = ImageModel({
         name: req.body.name,
         img: {
-          data: fs.readFileSync("./routes/uploads/" + req.file.originalname),
-          contentType: "image/png",
+            data: fs.readFileSync("./routes/uploads/" + req.file.originalname),
+            contentType: "image/png",
         },
-      });
-      const data=await saveImage
-      .save()
-      .then((res) => {
-        console.log("image is saved");
-      })
-      .catch((err) => {
-        console.log(err, "error has occur");
-      });
-
-      console.log(data) ;
-
-       res.send('image is saved')
-  
-
-    const newProduct=new ProductModel({})
+    });
+    //save the image first then save the product with the id of the image
+    saveImage
+        .save()
+        .then((ress) => {
+            const saveProduct = ProductModel({
+                name: "Product one",
+                price: "20",
+                discription: "this is the discription of product one",
+                ImageId: ress.id
+            })
+            saveProduct.save().then((ress) => {
+                //the product is saved
+                res.send(ress);
+            })
+        })
+        .catch((err) => {
+            console.log(err, "error has occur");
+        });
 }
 
 
+//get all the products
+const getProducts=async(req,res)=>{
+     ProductModel.find().then((ress)=>{
+        res.send(ress) ;
+     })
+    
+}
+
 //get the actuell user
 
-module.exports={
-    addProduct
-    
+module.exports = {
+    addProduct,
+    getProducts
+
 }
--- BackEnd/routes/ProductRoutes.js
+++ BackEnd/routes/ProductRoutes.js
@@ -6,7 +6,7 @@ const ImageModel=require('../models/ImageModel')
 const ProdutController=require('../Controllers/ProductController')
 const {upload}=require('../midleware/imageMidleware')
 router.post("/AddProduct",upload.single("testImage"),ProdutController.addProduct) ;
-
+router.get('/getProducts',ProdutController.getProducts)
     
 
     // const newImage= new ImageModel()
