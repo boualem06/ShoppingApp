fac9850b8d7aeb2442bc6ba016d83f4e36df0ea1
boualem06 <kb_hamroune@esi.dz>
2023-05-13 22:58:54 +0100

    the last commit

--- FrontEnd/src/components/List.js
+++ FrontEnd/src/components/List.js
@@ -19,86 +19,15 @@ import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
 
-export default function Item({cart,setCart,element,setElement}) {
+export default function Item({ cart, setCart, element, setElement }) {
 
 
-   
-    const [PizzaList, setPizzaList] = useState([
-        {
-            name: "Campagnoia",
-            price: "19.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-        {
-            name: "Neapolitan",
-            price: "22.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-        {
-            name: "Sicillian",
-            price: "18.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-        {
-            name: "St. Louis",
-            price: "22.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-        {
-            name: "Campagnoia",
-            price: "19.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-        {
-            name: "Neapolitan",
-            price: "22.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-        {
-            name: "Sicillian",
-            price: "18.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-        {
-            name: "St. Louis",
-            price: "22.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-        {
-            name: "Sicillian",
-            price: "18.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-        {
-            name: "Campagnoia",
-            price: "19.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-        {
-            name: "Neapolitan",
-            price: "22.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-        {
-            name: "Sicillian",
-            price: "18.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-        {
-            name: "St. Louis",
-            price: "22.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-        {
-            name: "Campagnoia",
-            price: "19.90",
-            description: "Lorem ipsum dolor sit amet, consectetur  elit "
-        },
-    ])
-    const [isClicked,setIsClicked]=useState(false)
-   
-    useEffect(()=>{
-        const loadProducts=async ()=>{
+
+    const [PizzaList, setPizzaList] = useState([])
+    const [isClicked, setIsClicked] = useState(false)
+
+    useEffect(() => {
+        const loadProducts = async () => {
             try {
                 const res = await fetch('http://localhost:5000/getProducts');
                 const data = await res.json();
@@ -108,37 +37,36 @@ export default function Item({cart,setCart,element,setElement}) {
                 console.error(err);
             }
         }
-        loadProducts() ;
-        
-    },[])
-    
+        loadProducts();
+
+    }, [])
+
 
     return (
 
+        <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 lg:px-8 md:px-4 px-2' >
+            {PizzaList.map((elem, index) => (
+                <div className='border shadow-md rounded-lg'>
+                    <Image style={{ height: "15rem", width: "100%" }} cloudName="dc7suzbrg" publicId={elem.imageUrl} />
 
-        // <div className='flex w-full'>
-            
-             <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4' >
-            {PizzaList.map((elem,index) => (
-                <div key={index}  className='shadow rounded-lg     flex flex-col justify-center items-center  py-1 border-2 '>
-                    {/* <img className='max-w-40 max-h-36 ' src={pizza} alt="Pizza image"></img> */}
-                    <Image
-                           key={index}
-                           cloudName={"dc7suzbrg"}
-                           publicId={elem.imageUrl}
-                           width="300"
-                           crop="scale"
-                       /> 
-                    <div className=' flex flex-col justify-center items-center'>
+
+                    <div className=' flex flex-col justify-center items-center w-full'>
                         <h1 className='text-lg font-bold text-red-500'>{elem.name}</h1>
-                        <h1 className='font-bold'>${elem.price}</h1>
-                        <button onClick={()=>{setElement(elem);setIsClicked(true)}} className=' mb-2 bg-orange-600 text-white px-2 py-1 rounded  hover:scale-105 hover:font-bold'>Add to Cart</button>
+                        <div className='flex justify-between   mt-4 items-center w-full'>
+                            <h1 className='font-bold text-3xl text-orange-600 ml-2'>${elem.price}.00</h1>
+                            <div className=' px-4'>
+                                <button onClick={() => { setElement(elem); setIsClicked(true) }} className=' mb-2 py-2 bg-orange-600 text-white px-2 py-1 rounded    text-lg  hover:shadow-xl '>Add to Cart</button>
+                            </div>
+                        </div>
+
                     </div>
                     {isClicked && <Navigate to={"/Detail"}></Navigate>}
-                </div>))}
+                </div>
+            ))
+            }
         </div>
-        // </div>
-       
+
+
 
 
 
